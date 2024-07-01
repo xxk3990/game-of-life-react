6f964ce94ea71d092fc43bbfd56b7c28bb6ea95f
Xander Kaylan <xxk3990@rit.edu>
2024-06-30 20:00:25 -0400

    IT WORKS + NEW FEATURES!

--- game-of-life/src/App.tsx
+++ game-of-life/src/App.tsx
@@ -14,16 +14,17 @@ const positions = [
 ]
 function App() {
   /* 
-    I followed this tutorial: https://dev.to/toluagboola/build-the-game-of-life-with-react-and-typescript-5e0d
+    https://www.youtube.com/watch?v=DvVt11mPuM0&ab_channel=BenAwad
     I did change some variable names that were in that tutorial and added a few new features of my own!
   */
   useEffect(() => {
     document.title = "Game of Life"
   })
   const rows = 25;
-  const [columns, setColumns] = useState<number>(35);
-  const [cellSize, setCellSize] = useState<number>(20);
-  const [isRunning, setIsRunning] = useState<boolean>(false);
+  const [columns, setColumns] = useState<number>(35); //adjustable columns
+  const [cellSize, setCellSize] = useState<number>(20); //adjustable cell size
+  const [isRunning, setIsRunning] = useState<boolean>(false); //playing or paused
+  const [darkModeOn, setDarkModeOn] = useState<boolean>(false); //boolean for dark mode
   const isRunningRef = useRef(isRunning);
   isRunningRef.current = isRunning;
   const runGame = useCallback(() => {
@@ -31,32 +32,29 @@ function App() {
       return; //bail out if game not running
     } 
     setGrid((g: number[][]) => {
-      return produce(g, (gridCopy: any) => {
+      console.log('set grid in run game reached')
+      return produce(g, (gridCopy) => {
         for (let i = 0; i < rows; i++) {
           for (let j = 0; j < columns; j++) {
             let neighbors = 0;
             positions.forEach(([x, y]) => {
-              const newI = i + x;
-                const newJ = j + y;
-                if (
-                  newI >= 0 &&
-                  newI < rows &&
-                  newJ >= 0 &&
-                  newJ < columns
-                ) {
-                  neighbors += g[newI][newJ];
-                }
+              const newX = i + x; //nearby cells
+              const newY = j + y;
+              if (newX >= 0 && newX < rows && newY >= 0 && newY < columns) {
+                neighbors += g[newX][newY];
+              }
             });
-            if (neighbors < 2 || neighbors > 3) {
-              gridCopy[i][j] = 0;
+
+            if (neighbors < 2 || neighbors > 3) { 
+              gridCopy[i][j] = 0; //kill cell if neighbors in this range
             } else if (g[i][j] === 0 && neighbors === 3) {
               gridCopy[i][j] = 1;
             }
           }
         }
-      })
+      });
     })
-    setTimeout(runGame, 100)
+    setTimeout(runGame, 200)
   }, [])
 
   const randomCellColor = () => {
@@ -93,9 +91,17 @@ function App() {
     setCellSize(newCellSize)
   }
 
+  const randomGrid = () => {
+    const randomRows = []
+    for (let i = 0; i < rows; i++) {
+      //initialize grid with random setup
+      randomRows.push(Array.from(Array(columns), () => (Math.random() > 0.7 ? 1 : 0)));
+    }
+    return randomRows;
+  }
 
-  const [grid, setGrid] = useState<any>(() => {
-    const emptyRows = []
+  const [grid, setGrid] = useState(() => {
+    const emptyRows = [] //initialize grid
     for(let i = 0; i < rows; i++) {
       emptyRows.push(Array.from(Array(columns), () => 0))
     }
@@ -105,27 +111,35 @@ function App() {
     <div className="App">
       <h1>Game of Life – Xander K</h1>
       <section className='UI'> {/* UI for adjusting board */}
-        <button onClick={() => handlePlay()}>{isRunning ? 'Pause' : 'Play'}</button>
-        <button onClick={() => setGrid(clearGrid)}>Clear Grid</button>
-        <span>Change Column #: <input type="range" min="10" step="5" max="40" value={columns} onChange={(e) => changeNumColumns(parseInt(e.target.value))}/> Columns: {columns}</span>
-        <span>Change Cell Size: <input type ='range' min="10" max="40" value={cellSize} step="5" onChange={(e) => changeCellSize(parseInt(e.target.value))}/> Cell size: {cellSize} x {cellSize}</span>
+        <section className="UI-btns">
+          <button onClick={() => handlePlay()}>{isRunning ? 'Pause' : 'Play'}</button>
+          <button onClick={() => setGrid(clearGrid)}>Clear Grid</button>
+          <button onClick={() => setGrid(randomGrid)}>Random Grid</button>
+          <button onClick={() => setDarkModeOn(!darkModeOn)}>{darkModeOn ? 'Light Mode' : 'Dark Mode'}</button>
+        </section>
+        <section className="UI-ranges">
+          <span>Change Column #: <input type="range" min="10" step="5" max="40" value={columns} onChange={(e) => changeNumColumns(parseInt(e.target.value))}/> Columns: {columns}</span>
+          <span>Change Cell Size: <input type ='range' min="10" max="40" value={cellSize} step="5" onChange={(e) => changeCellSize(parseInt(e.target.value))}/> Cell size: {cellSize} x {cellSize}</span>
+        </section>
       </section>
-      <section className="grid-of-cells" style={{
+      <section style={{
         display: 'grid',
         gridTemplateColumns: `repeat(${columns}, 1fr)`,
+        backgroundColor: darkModeOn ? `black` : 'white'
       }}>
-        {grid.map((rws: any, i: any) =>
-          rws.map((col: any, j: any) => (
+        {grid.map((rws: any, x: any) =>
+          rws.map((col: any, y: any) => (
             <section className="cell"
-              key={`${i}-${j}`}
+              key={`${x}-${y}`}
               style={{
                 width: cellSize,
                 height: cellSize,
-                backgroundColor: grid[i][j] ? randomCellColor() : undefined,
+                backgroundColor: grid[x][y] ? 'blue' : undefined,
+                border: darkModeOn ? '1px solid white' : '1px solid black'
               }}
               onClick={() => {
-                const newGrid = produce(grid, (gridCopy: any) => {
-                  gridCopy[i][j] = grid[i][j] ? 0 : 1;
+                const newGrid = produce(grid,(gridCopy) => {
+                  gridCopy[x][y] = grid[x][y] ? 0 : 1;
                 });
                 setGrid(newGrid);
               }}
@@ -133,7 +147,6 @@ function App() {
           ))
         )}
       </section>
-        
     </div>
   );
 }
